43fab618bfb7c282a25f87570057fee5a579621d
StephenTan12 <sttan@ucsd.edu>
2022-12-01 13:29:52 -0800

    added support for local runs

--- src/queries.ts
+++ src/queries.ts
@@ -1,23 +1,10 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
 import errors from './errors';
+import data from './data';
+import formatResults from './utils';
 
 dotenv.config();
-
-// Constants
-const PRODUCTION_CONSTANTS = {
-  "steelIron": 1.39,
-  "cement": 0.59,
-  "plastic": 2.6,
-  "glass": 0.54,
-  "latex": 0.54,
-  "rubber": 0.74,
-  "copper": 0.2,
-  "silver": 196,
-  "aluminum": 10.14,
-  "textile": 25,
-  "paper": 0.0028
-}
 
 // Database
 const pool: pg.Pool = new pg.Pool({
@@ -35,7 +22,10 @@ const getItem = async (req, res, next) => {
     next(errors.ERRORS.ItemIdNotInt);
   }
 
-  console.log("querying item")
+  if (process.env.RUN_LOCALLY === "TRUE") {
+    res.status(200).json(formatResults(data[String(id)]));
+    return
+  }
 
   await pool
     .query('SELECT * FROM items LEFT JOIN production_stats USING (id) LEFT JOIN electricity_stats USING (id) WHERE id = $1;', [id])
@@ -44,56 +34,10 @@ const getItem = async (req, res, next) => {
         next(errors.ERRORS.ItemNotFound);
       }
       const unformattedResults = results.rows[0]
-      const productionStats = {
-        "steelIron": unformattedResults.steeliron,
-        "cement": unformattedResults.cement,
-        "plastic": unformattedResults.plastic,
-        "glass": unformattedResults.glass,
-        "latex": unformattedResults.latex,
-        "rubber": unformattedResults.rubber,
-        "copper": unformattedResults.copper,
-        "silver": unformattedResults.silver,
-        "aluminum": unformattedResults.aluminum,
-        "textile": unformattedResults.textile,
-        "paper": unformattedResults.paper
-      }
-
-      let productionCost = 0
-      const productionArray = []
-      for (const [key, value] of Object.entries(productionStats)) {
-        const cost = value * PRODUCTION_CONSTANTS[key]
-        productionCost += cost
-        productionArray.push([key, cost])
-      }
-
-      productionArray.sort((l, r) => {
-        if (l[1] > r[1]) {
-          return -1
-        } else if (l[1] < r[1]) {
-          return 1
-        }
-        return 0
-      })
-
-      const topFiveProduction = productionArray.slice(0, 5)
-      const electricalCost = unformattedResults.electricitykwh * 0.001183 * 8760
-
-      const formattedResults = {
-        "id": unformattedResults.id,
-        "name": unformattedResults.name,
-        "imgid": unformattedResults.imgid,
-        "description": unformattedResults.description,
-        "productionStats": productionStats,
-        "productionCost": productionCost,
-        "productionTopFive": topFiveProduction,
-        "electricityKWH": unformattedResults.electricitykwh,
-        "electricityCost": electricalCost
-      }
+      const formattedResults = formatResults(unformattedResults);
       res.status(200).json(formattedResults);
     })
     .catch((err) => {
-      console.log(err)
-
       next(err);
     });
 };
